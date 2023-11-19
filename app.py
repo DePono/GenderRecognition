95fb2cad74fae3b79e58b103ac227662e3a33fbd
Denis <maestrodark@icloud.com>
2023-11-19 22:32:26 +0400

    Готово для диплома

--- app.py
+++ app.py
@@ -1,35 +1,70 @@
 import time
 
+import librosa
 import streamlit as st
+import numpy as np
+
+from main import create_model
+
+
+def extract_feature(file_name, **kwargs):
+    """
+    Extract feature from audio file `file_name`
+        Features supported:
+            - MFCC (mfcc)
+            - Chroma (chroma)
+            - MEL Spectrogram Frequency (mel)
+            - Contrast (contrast)
+            - Tonnetz (tonnetz)
+        e.g:
+        `features = extract_feature(path, mel=True, mfcc=True)`
+    """
+    mfcc = kwargs.get("mfcc")
+    chroma = kwargs.get("chroma")
+    mel = kwargs.get("mel")
+    contrast = kwargs.get("contrast")
+    tonnetz = kwargs.get("tonnetz")
+    X, sample_rate = librosa.core.load(file_name)
+    if chroma or contrast:
+        stft = np.abs(librosa.stft(X))
+    result = np.array([])
+    if mfcc:
+        mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+        result = np.hstack((result, mfccs))
+    if chroma:
+        chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+        result = np.hstack((result, chroma))
+    if mel:
+        mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T, axis=0)
+        result = np.hstack((result, mel))
+    if contrast:
+        contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T, axis=0)
+        result = np.hstack((result, contrast))
+    if tonnetz:
+        tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T, axis=0)
+        result = np.hstack((result, tonnetz))
+    return result
+
 
 st.header("Распознавание пола по голосу")
-
 
 uploaded_file = st.file_uploader(label="Пожалуйста, загрузите Ваш файл в формате wav")
 if uploaded_file:
     audio = uploaded_file.getvalue()
     st.audio(audio)
-    result = {}
-    # polling
-    sleep_duration = 1
-    percent_complete = 0
-    progress_bar = st.progress(percent_complete)
-    st.text("Currently in queue")
-    while result.get("status") != "processing":
-        percent_complete += sleep_duration
-        time.sleep(sleep_duration)
-        progress_bar.progress(percent_complete / 10)
 
-    sleep_duration = 0.01
 
-    for percent in range(percent_complete, 101):
-        time.sleep(sleep_duration)
-        progress_bar.progress(percent)
 
-    with st.spinner("Processing....."):
-        while result.get("status") != 'completed':
-            result = get_text(token, t_id)
-
+    # construct the model
+    model = create_model()
+    # load the saved/trained weights
+    model.load_weights("results/model.h5")
+    # extract features and reshape it
+    features = extract_feature(uploaded_file, mel=True).reshape(1, -1)
+    # predict the gender!
+    male_prob = model.predict(features)[0][0]
+    female_prob = 1 - male_prob
+    gender = "male" if male_prob > female_prob else "female"
     st.balloons()
-    st.header("Transcribed Text")
-    st.subheader(result['text'])
+    st.header("Определен пол")
+    st.subheader(f"Вероятность:     Мужчина: {male_prob * 100:.2f}%    Женщина: {female_prob * 100:.2f}%")
